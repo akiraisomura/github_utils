3831007b1ca122af7a21d5fd056d029826dd9500
akiraisomura <a.isomura5160@gmail.com>
2024-03-22 15:38:02 +0900

    feat: fix .title -> [:content][:title]

--- check_story_point_in_the_title_v4.rb
+++ check_story_point_in_the_title_v4.rb
@@ -19,7 +19,7 @@ def main
 end
 
 def check_story_point_in_the_title(issue)
-  return if issue.title.match(/\[\d+\]/)
+  return if issue[:content][:title].match(/\[\d+\]/)
 
   mention_target = decide_mention_target(issue)
   github_client.add_comment(repository, issue[:content][:number], "@#{mention_target} #{config['MESSAGE']}")
